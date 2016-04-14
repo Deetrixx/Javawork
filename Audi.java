916a96832c6af675499080b9fc98d1d042a3da8b
Deetrixx <dennisgithua30@gmail.com>
2016-04-14 05:41:23 +0300

    extends from Car main class

---
+++ Audi.java
@@ -0,0 +1,18 @@
+
+public class Audi {
+	public static void Accelerate(String x){
+		 System.out.println("Driving is fun.");
+	}
+	public static void Cruising(){
+		System.out.println("Its normally liked for its cruising efficiency");
+	}
+	public static void main(String [] args){
+	Car k-new Car();
+	Audi m=new Audi();
+	k.Accelerate();
+	m.Accelarate();
+	k.Accelerate(120,4);
+	m.Accelerate(160,9);
+		
+	}
+}
